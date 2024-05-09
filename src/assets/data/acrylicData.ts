682a5c7bd85cb6b599e09b388fb857319d221fc9
pierrepinoli <pinoli.pierre@gmail.com>
2024-05-09 12:00:16 +0200

    importation des images acrylique et huile dans leurs data

--- src/assets/data/acrylicData.ts
+++ src/assets/data/acrylicData.ts
@@ -3,6 +3,38 @@
 const golf = "/images/Acrylique/golf.jpg";
 const new_york_vintage = "/images/Acrylique/new_york_vintage.jpg";
 
+const africa_mask = "/images/Acrylique/africa_mask.jpg";
+const african_woman = "/images/Acrylique/african_woman.jpg";
+const african_woman_2 = "/images/Acrylique/african_woman_2.jpg";
+const african_woman_3 = "/images/Acrylique/african_woman_3.jpg";
+
+const bowl_2 = "/images/Acrylique/bowl_2.jpg";
+const bowl_sold = "/images/Acrylique/bowl_sold.jpg";
+
+const chicken = "/images/Acrylique/chicken.jpg";
+const city = "/images/Acrylique/city.jpg";
+const cups = "/images/Acrylique/cups.jpg";
+const farfalle = "/images/Acrylique/farfalle.jpg";
+const glass_of_wine_sold = "/images/Acrylique/glass_of_wine_sold.jpg";
+const gondola = "/images/Acrylique/gondola.jpg";
+
+const ice_cream_sold = "/images/Acrylique/ice_cream_sold.jpg";
+const jar = "/images/Acrylique/jar.jpg";
+const lancer = "/images/Acrylique/lancer.jpg";
+const lion = "/images/Acrylique/lion.jpg";
+const new_york_1 = "/images/Acrylique/new_york_1.jpg";
+const new_york_vintage_2 = "/images/Acrylique/new_york_vintage_2.jpg";
+
+const planets = "/images/Acrylique/planets.jpg";
+const rhinoceros = "/images/Acrylique/rhinoceros.jpg";
+const school_bag = "/images/Acrylique/school_bag.jpg";
+const ski_sold = "/images/Acrylique/ski_sold.jpg";
+const tajine = "/images/Acrylique/tajine.jpg";
+const terracotta_pots = "/images/Acrylique/terracotta_pots.jpg";
+
+const tulips = "/images/Acrylique/tulips.jpg";
+const vespa = "/images/Acrylique/vespa.jpg";
+
 const filters = {
   sold: "Vendu",
   available: "Disponible",
@@ -20,7 +52,189 @@ const acrylicData = [
     title: "New York",
     imageOriginal: new_york_vintage,
     ariaLabel: "",
-    alt: "peinture représentant New York",
+    alt: "peinture représentant la ville de New York",
+    available: true,
+  },
+  {
+    title: "Masque africain",
+    imageOriginal: africa_mask,
+    ariaLabel: "",
+    alt: "peinture d'un masque africain",
+    available: true,
+  },
+  {
+    title: "Femme africaine",
+    imageOriginal: african_woman,
+    ariaLabel: "",
+    alt: "peinture d'un femme africaine",
+    available: true,
+  },
+  {
+    title: "Femme africaine",
+    imageOriginal: african_woman_2,
+    ariaLabel: "",
+    alt: "peinture d'un femme africaine",
+    available: true,
+  },
+  {
+    title: "Femme africaine",
+    imageOriginal: african_woman_3,
+    ariaLabel: "",
+    alt: "peinture d'un femme africaine",
+    available: true,
+  },
+  {
+    title: "Bols",
+    imageOriginal: bowl_2,
+    ariaLabel: "",
+    alt: "peinture représentant des bols de differentes couleurs",
+    available: true,
+  },
+  {
+    title: "Bols",
+    imageOriginal: bowl_sold,
+    ariaLabel: "",
+    alt: "peinture représentant des bols de differentes couleurs",
+    available: false,
+  },
+  {
+    title: "Poulette",
+    imageOriginal: chicken,
+    ariaLabel: "",
+    alt: "peinture d'une poulette en train de balayer",
+    available: true,
+  },
+  {
+    title: "Ville",
+    imageOriginal: city,
+    ariaLabel: "",
+    alt: "peinture représentant une ville",
+    available: true,
+  },
+  {
+    title: "Tasses",
+    imageOriginal: cups,
+    ariaLabel: "",
+    alt: "peinture représentant des tasses de differentes couleurs",
+    available: true,
+  },
+  {
+    title: "Farfalle",
+    imageOriginal: farfalle,
+    ariaLabel: "",
+    alt: "peinture représentant des farfalles",
+    available: true,
+  },
+  {
+    title: "Verres de vin",
+    imageOriginal: glass_of_wine_sold,
+    ariaLabel: "",
+    alt: "peinture représentant des verres de vins",
+    available: false,
+  },
+  {
+    title: "Gondoles",
+    imageOriginal: gondola,
+    ariaLabel: "",
+    alt: "peinture représentant des gondoles sur un canal urbain",
+    available: true,
+  },
+  {
+    title: "Glaces",
+    imageOriginal: ice_cream_sold,
+    ariaLabel: "",
+    alt: "peinture représentant des batonnets de glace",
+    available: false,
+  },
+  {
+    title: "Jarres",
+    imageOriginal: jar,
+    ariaLabel: "",
+    alt: "peinture représentant des jarres de couleurs différentes",
+    available: true,
+  },
+  {
+    title: "Lancier",
+    imageOriginal: lancer,
+    ariaLabel: "",
+    alt: "peinture représentant un lancier tenant sa lance",
+    available: true,
+  },
+  {
+    title: "Lion",
+    imageOriginal: lion,
+    ariaLabel: "",
+    alt: "portrait d'un lion",
+    available: true,
+  },
+  {
+    title: "New York",
+    imageOriginal: new_york_1,
+    ariaLabel: "",
+    alt: "peinture représentant la ville de New York",
+    available: true,
+  },
+  {
+    title: "New York",
+    imageOriginal: new_york_vintage_2,
+    ariaLabel: "",
+    alt: "peinture représentant la ville de New York",
+    available: true,
+  },
+  {
+    title: "Planètes",
+    imageOriginal: planets,
+    ariaLabel: "",
+    alt: "peinture représentant des planètes",
+    available: true,
+  },
+  {
+    title: "Rhinocéros",
+    imageOriginal: rhinoceros,
+    ariaLabel: "",
+    alt: "peinture d'un rhinocéros",
+    available: true,
+  },
+  {
+    title: "Porte-manteau d'écolier",
+    imageOriginal: school_bag,
+    ariaLabel: "",
+    alt: "peinture d'un porte-manteau d'écolier",
+    available: true,
+  },
+  {
+    title: "Ski",
+    imageOriginal: ski_sold,
+    ariaLabel: "",
+    alt: "peinture d'un skieur",
+    available: false,
+  },
+  {
+    title: "Tajines",
+    imageOriginal: tajine,
+    ariaLabel: "",
+    alt: "peinture représentant des tajines de couleurs différentes",
+    available: true,
+  },
+  {
+    title: "Pots en terre cuite",
+    imageOriginal: terracotta_pots,
+    ariaLabel: "",
+    alt: "peinture de pots en terre cuite",
+    available: true,
+  },
+  {
+    title: "Tulipes",
+    imageOriginal: tulips,
+    ariaLabel: "",
+    alt: "peinture représentant des tulipes",
+    available: true,
+  },
+  {
+    title: "PVespa",
+    imageOriginal: vespa,
+    ariaLabel: "",
+    alt: "peinture d'un vespa",
     available: true,
   },
 ];
--- src/assets/data/oilData.ts
+++ src/assets/data/oilData.ts
@@ -5,6 +5,36 @@ const bathers = "/images/Huile/bathers_sold.jpg";
 const beach_huts = "/images/Huile/beach_huts_sold.jpg";
 const cable_car = "/images/Huile/cable_car.jpg";
 
+const boat_sold = "/images/Huile/boat_sold.jpg";
+const buoy_boat = "/images/Huile/buoy_boat.jpg";
+const cow = "/images/Huile/cow.jpg";
+const deckchair = "/images/Huile/deckchair.jpg";
+
+const fish = "/images/Huile/fish.jpg";
+const fishes = "/images/Huile/fishes.jpg";
+const hut_1 = "/images/Huile/hut_1.jpg";
+const hut_2 = "/images/Huile/hut_2.jpg";
+
+const pens = "/images/Huile/pens.jpg";
+const pilchards = "/images/Huile/pilchards.jpg";
+const pots_of_rice = "/images/Huile/pots_of_rice.jpg";
+const sea_side = "/images/Huile/sea_side.jpg";
+
+const seahorse = "/images/Huile/seahorse.jpg";
+const slippers = "/images/Huile/slippers.jpg";
+const tajine_sold = "/images/Huile/tajine_sold.jpg";
+const tongue = "/images/Huile/tongue.jpg";
+
+const van_sold = "/images/Huile/van_sold.jpg";
+const venice = "/images/Huile/venice.jpg";
+const vespa = "/images/Huile/vespa.jpg";
+const vespa_2 = "/images/Huile/vespa_2.jpg";
+
+const vespa_3 = "/images/Huile/vespa_3.jpg";
+const vespa_4 = "/images/Huile/vespa_4.jpg";
+const vespa_5 = "/images/Huile/vespa_5.jpg";
+const vintage_car = "/images/Huile/vintage_car.jpg";
+
 const filters = {
   sold: "Vendu",
   available: "Disponible",
@@ -39,75 +69,172 @@ const oilData = [
     alt: "peinture de télécabine",
     available: true,
   },
-  // ******************** //
   {
-    title: "Panorama asiatique",
-    imageOriginal: asian_panorama,
+    title: "Bateau",
+    imageOriginal: boat_sold,
     ariaLabel: "",
-    alt: "peinture d'un panorama d'une côte asiatique",
+    alt: "peinture d'un bateau",
+    available: false,
+  },
+  {
+    title: "Bouées de mouillage",
+    imageOriginal: buoy_boat,
+    ariaLabel: "",
+    alt: "peinture de bouées de mouillage",
     available: true,
   },
   {
-    title: "Télécabines",
-    imageOriginal: cable_car,
+    title: "Vache",
+    imageOriginal: cow,
     ariaLabel: "",
-    alt: "peinture de télécabine",
+    alt: "peinture d'un vache",
     available: true,
   },
   {
-    title: "Panorama asiatique",
-    imageOriginal: asian_panorama,
+    title: "Transat",
+    imageOriginal: deckchair,
     ariaLabel: "",
-    alt: "peinture d'un panorama d'une côte asiatique",
+    alt: "peinture de transat",
     available: true,
   },
   {
-    title: "Télécabines",
-    imageOriginal: cable_car,
+    title: "Poisson",
+    imageOriginal: fish,
     ariaLabel: "",
-    alt: "peinture de télécabine",
+    alt: "peinture d'un poisson dans une assiette",
     available: true,
   },
   {
-    title: "Panorama asiatique",
-    imageOriginal: asian_panorama,
+    title: "Poissons",
+    imageOriginal: fishes,
     ariaLabel: "",
-    alt: "peinture d'un panorama d'une côte asiatique",
+    alt: "peinture de poissons en train de sècher",
     available: true,
   },
   {
-    title: "Télécabines",
-    imageOriginal: cable_car,
+    title: "Cabane",
+    imageOriginal: hut_1,
     ariaLabel: "",
-    alt: "peinture de télécabine",
+    alt: "peinture d'un paysage avec une cabane",
     available: true,
   },
   {
-    title: "Panorama asiatique",
-    imageOriginal: asian_panorama,
+    title: "Cabane",
+    imageOriginal: hut_2,
     ariaLabel: "",
-    alt: "peinture d'un panorama d'une côte asiatique",
+    alt: "peinture d'un paysage avec une cabane",
     available: true,
   },
   {
-    title: "Télécabines",
-    imageOriginal: cable_car,
+    title: "Crayons",
+    imageOriginal: pens,
     ariaLabel: "",
-    alt: "peinture de télécabine",
+    alt: "peinture représentant des crayons",
     available: true,
   },
   {
-    title: "Panorama asiatique",
-    imageOriginal: asian_panorama,
+    title: "Boites de conserves",
+    imageOriginal: pilchards,
     ariaLabel: "",
-    alt: "peinture d'un panorama d'une côte asiatique",
+    alt: "peinture de boites de conserves",
     available: true,
   },
   {
-    title: "Télécabines",
-    imageOriginal: cable_car,
+    title: "Pots de riz",
+    imageOriginal: pots_of_rice,
     ariaLabel: "",
-    alt: "peinture de télécabine",
+    alt: "peinture de pots de riz",
+    available: true,
+  },
+  {
+    title: "Bord de mer",
+    imageOriginal: sea_side,
+    ariaLabel: "",
+    alt: "peinture d'un paysage de bord de mer",
+    available: true,
+  },
+  {
+    title: "Hippocampe",
+    imageOriginal: seahorse,
+    ariaLabel: "",
+    alt: "peinture d'un hippocampe",
+    available: true,
+  },
+  {
+    title: "Babouches",
+    imageOriginal: slippers,
+    ariaLabel: "",
+    alt: "peinture représentant des babouches",
+    available: true,
+  },
+  {
+    title: "Tajines",
+    imageOriginal: tajine_sold,
+    ariaLabel: "",
+    alt: "peinture représentant des tajines de couleurs différentes",
+    available: false,
+  },
+  {
+    title: "Tongues",
+    imageOriginal: tongue,
+    ariaLabel: "",
+    alt: "peinture représentant des tongues de couleurs différentes",
+    available: true,
+  },
+  {
+    title: "Van",
+    imageOriginal: van_sold,
+    ariaLabel: "",
+    alt: "peinture d'un van",
+    available: false,
+  },
+  {
+    title: "Venise",
+    imageOriginal: venice,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: true,
+  },
+  {
+    title: "Vespa",
+    imageOriginal: vespa,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: false,
+  },
+  {
+    title: "Vespa",
+    imageOriginal: vespa_2,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: true,
+  },
+  {
+    title: "Vespa",
+    imageOriginal: vespa_3,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: false,
+  },
+  {
+    title: "Vespa",
+    imageOriginal: vespa_4,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: true,
+  },
+  {
+    title: "Vespa",
+    imageOriginal: vespa_5,
+    ariaLabel: "",
+    alt: "peinture représentant un vespa",
+    available: true,
+  },
+  {
+    title: "Vieille voiture",
+    imageOriginal: vintage_car,
+    ariaLabel: "",
+    alt: "peinture représentant une vieille voiture",
     available: true,
   },
 ];
